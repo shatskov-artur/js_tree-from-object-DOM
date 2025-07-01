6e74f29ca18a14aa87ca4882e29f55e4efeb7d46
Artur Shatskov <artur.shackov77@gmail.com>
2025-07-01 22:53:45 +0200

    make build

---
+++ js_tree-from-object-DOM.f75de5e1.js
@@ -0,0 +1,34 @@
+'use strict';
+// const { createElement } = require('react');
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector('#tree');
+function createTree(element, data) {
+    if (!data || typeof data !== 'object') return;
+    const ul = document.createElement('ul');
+    for(const key in data){
+        const li = document.createElement('li');
+        li.textContent = key;
+        const child = data[key];
+        if (child && Object.keys(child).length > 0) createTree(li, child);
+        ul.appendChild(li);
+    }
+    element.appendChild(ul);
+}
+createTree(tree, food);
+
+//# sourceMappingURL=js_tree-from-object-DOM.f75de5e1.js.map
